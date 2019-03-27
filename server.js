7dc4927f4c21d55e8187035bb66e26200b91296a
Andrew Nelson <andrewnelson@ibm.com>
2019-03-26 18:21:12 -0600

    trying application/xml

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var jsonxml = require('jsontoxml');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.type('Content-Type', 'text/xml');
+    res.type('Content-Type', 'application/xml');
     request.get({ url: "http://api.wbur.org/events", json: "true" }, function(error, response, body) {
         if (!error && response.statusCode == 200) {
           res.send(jsonxml(body));
